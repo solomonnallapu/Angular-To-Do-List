316f4dfb4351b55f8361a6438bdc6645b27fc30c
sys2612 <nageswar@way2online.net>
2019-04-16 18:16:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'To-do-List';
+  allTasks:any=[];
+  newTask:any;
+  addNewTaskone=false;
+  addNewTask=true;
+  todos:any=[];
+
+
+  constructor(){}
+
+  onAdd(){
+this.addNewTaskone = !this.addNewTaskone;
+// this.allTasks=this.newTask;
+// this.allTasks.push(this.newTask);
+console.log('add new task',this.addNewTaskone);
+  }
+
+  taskEntered(){
+    this.addNewTaskone = !this.addNewTaskone;
+// this.allTasks=this.newTask;
+this.allTasks.push(this.newTask);
+console.log('Enter new task');
+  }
 }
