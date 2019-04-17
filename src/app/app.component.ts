d4fffc35e1a7882ec76747f1b3216dc010a949d5
sys2612 <nageswar@way2online.net>
2019-04-17 18:44:25 +0530

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'To-do-List';
-  allTasks:any=[];
-  newTask:any;
-  addNewTaskone=false;
-  addNewTask=true;
-  todos:any=[];
+  allTasks: any = [];
+  newTask: any;
+  addNewTaskone = false;
+  addNewTask = true;
+  todos: any = [];
 
+  constructor() { }
 
-  constructor(){}
-
-  onAdd(){
-this.addNewTaskone = !this.addNewTaskone;
-// this.allTasks=this.newTask;
-// this.allTasks.push(this.newTask);
-console.log('add new task',this.addNewTaskone);
+  onAdd() {
+    this.addNewTaskone = !this.addNewTaskone;
+    console.log('add new task', this.addNewTaskone);
   }
 
-  taskEntered(){
+  submitTask() {
     this.addNewTaskone = !this.addNewTaskone;
-// this.allTasks=this.newTask;
-this.allTasks.push(this.newTask);
-console.log('Enter new task');
+    this.allTasks.push(this.newTask);
+    console.log('Enter new task', this.addNewTaskone);
+    this.newTask = '';
+
+  }
+  onClose(){
+    this.addNewTaskone = false;
   }
 }
