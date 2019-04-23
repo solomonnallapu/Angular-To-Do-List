f1b520a1e4722e4a88c08c1b8050561edbcf8e93
sys2612 <nageswar@way2online.net>
2019-04-23 12:16:14 +0530

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DropEvent } from 'angular-draggable-droppable';
 
 @Component({
   selector: 'app-root',
@@ -9,29 +10,81 @@ export class AppComponent {
   title = 'To-do-List';
   allTasks: any = [];
   newTask: any;
+  newTaskSelect=true;
   date = new Date(Date.now());
   addNewTaskone = false;
   addNewTask = true;
+  rotateAnimi=false;
   todos: any = [];
+  selectIndex:any;
+  //Dragable Variables
+  onStart=false;
+  indexValue:any;
+  droppedData: string = '';
 
   constructor() {
-    setTimeout(()=>this.addNewTaskone=true, 10000);
-   }
 
+  }
+  // Go To Add New Task
   onAdd() {
     this.addNewTaskone = !this.addNewTaskone;
     console.log('add new task', this.addNewTaskone);
-  }
-
-  submitTask() {
-    this.addNewTaskone = !this.addNewTaskone;
-    this.allTasks.push(this.newTask);
-    console.log('Enter new task', this.addNewTaskone);
     this.newTask = '';
-
   }
-  onClose(){
-    this.addNewTaskone = false;
-  }
+ // Select Task
+ onSelect(task, i){
+  this.newTask=task;
+  this.addNewTaskone=!this.addNewTaskone;
+  this.newTaskSelect=false;
+  console.log('idhiOkaKothaTask Value',this.newTaskSelect);
   
+  console.log('task is',this.newTask);
+  console.log('Clicked on Task',this.selectIndex);
+  return this.selectIndex=i;
+ }
+// Edit Task
+  submitTask(newTask) {
+    if((this.selectIndex || this.selectIndex === 0 ) && !this.newTaskSelect ){
+      this.allTasks[this.selectIndex]=newTask;
+      console.log('All tasks array', this.allTasks[this.selectIndex]);
+      console.log('idhiOkaKothaTask Value',this.newTaskSelect);
+      }
+    else if(this.newTaskSelect){
+      this.allTasks.push(this.newTask);
+    }
+    this.addNewTaskone = false;
+    console.log('Enter new task', this.addNewTaskone);
+    console.log('Task Submit is',newTask);
+    this.newTask = '';
+  }
+ 
+  onClose() {
+    this.rotateAnimi = true;
+    console.log("animate", this.rotateAnimi);
+    setTimeout(() => {
+      this.addNewTaskone = false;
+      this.rotateAnimi = false;
+    }, 500);
+  }
+
+  // Drag Start
+  dragStart(){
+    this.onStart=!this.onStart;
+    console.log('Drag started',this.onStart);
+  }
+// DRAG and Release
+  dragEnd(i){
+this.indexValue=i;
+console.log('Index Value is',this.indexValue);
+this.onStart=false;
+console.log('onStart Value is',this.onStart);
+return this.indexValue;
+  }
+// Drop and Delete
+  onDrop({ dropData }: DropEvent<string>): void {
+  console.log('Droped');
+  this.allTasks.splice(this.indexValue, 1);
+  this.onStart=false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { DragAndDropModule } from 'angular-draggable-droppable';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DragAndDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
